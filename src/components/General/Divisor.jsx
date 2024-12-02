f297a7728a139c9b90d4788caf060cb98165241f
Mathias <mncianni@hotmail.com>
2024-12-02 17:23:14 -0300

    fix: divisor

--- src/components/General/Divisor.jsx
+++ src/components/General/Divisor.jsx
@@ -1,6 +1,6 @@
 const Divisor = () => {
     return (
-        <div className="flex items-center gap-2 max-w-[50%] mx-auto my-desk">
+        <div className="flex items-center gap-2 max-w-[50%] my-32 lg:my-desk">
             <div className="h-[5px] w-[5px] aspect-square rounded-full bg-darkAccent"></div>
             <div className="h-[1px] w-full bg-darkAccent"></div>
             <div className="h-[5px] w-[5px] aspect-square rounded-full bg-darkAccent"></div>
